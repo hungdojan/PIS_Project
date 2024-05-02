35fa8715cc432ba34a6262c84bdbe4e515dc0853
Magdalena Bellayova <62183961+xbella01@users.noreply.github.com>
2024-05-02 13:30:56 +0200

    Add createDrink

--- pis-backend/src/main/java/cz/vut/fit/pisbackend/api/DrinksAPI.java
+++ pis-backend/src/main/java/cz/vut/fit/pisbackend/api/DrinksAPI.java
@@ -26,5 +26,26 @@ public class DrinksAPI {
     public List<DrinkDTO> getAll() {
         return drinkMgr.findAll().stream().map(x -> new DrinkDTO(x)).toList();
     }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createDrink(DrinkDTO drink) {
+        Drink existing = drinkMgr.find(drink.getId());
+        if (existing == null){
+            Drink d = new Drink();
+            d.setName(drink.getName());
+            d.setDescription(drink.getDescription());
+            d.setPrice(drink.getPrice());
+            d.setType(drink.getType());
+            d.setVolume(drink.getVolume());
+            Drink savedDrink = drinkMgr.create(d);
+            final URI uri = UriBuilder.fromPath("/drinks/{resourceServerId}").build(savedDrink.getId());
+            return Response.created(uri).entity(savedDrink).build();
+        }
+        else{
+            return Response.status(Response.Status.CONFLICT).entity(new ResponseMessageDTO("duplicate id")).build();
+        }
+    }
 
 }
