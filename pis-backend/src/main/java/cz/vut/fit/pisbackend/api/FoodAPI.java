d3abd2a837287f13bc4bb0bd24950cdf85531c14
Magdalena Bellayova <62183961+xbella01@users.noreply.github.com>
2024-05-02 13:27:49 +0200

    Add createFood

--- pis-backend/src/main/java/cz/vut/fit/pisbackend/api/FoodAPI.java
+++ pis-backend/src/main/java/cz/vut/fit/pisbackend/api/FoodAPI.java
@@ -27,4 +27,25 @@ public class FoodAPI {
         return foodMgr.findAll().stream().map(x -> new FoodDTO(x)).toList();
     }
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createFood(FoodDTO food) {
+        Food existing = foodMgr.find(food.getId());
+        if (existing == null){
+            Food f = new Food();
+            f.setName(food.getName());
+            f.setDescription(food.getDescription());
+            f.setPrice(food.getPrice());
+            f.setType(food.getType());
+            f.setAllergens(food.getAllergens());
+            f.setGrams(food.getGrams());
+            Food savedFood = foodMgr.create(f);
+            final URI uri = UriBuilder.fromPath("/foods/{resourceServerId}").build(savedFood.getId());
+            return Response.created(uri).entity(savedFood).build();
+        }
+        else{
+            return Response.status(Response.Status.CONFLICT).entity(new ResponseMessageDTO("duplicate id")).build();
+        }
+    }
 }
