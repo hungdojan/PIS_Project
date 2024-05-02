eed9e0d41496786b91ce3613b0c688bd7ce4f683
Magdalena Bellayova <62183961+xbella01@users.noreply.github.com>
2024-05-02 15:07:13 +0200

    Add food drink (#17)
    * Add createFood
    
    * Add createDrink
    
    * Fix up
    
    * Add DTO constructor

--- pis-backend/src/main/java/cz/vut/fit/pisbackend/api/DrinksAPI.java
+++ pis-backend/src/main/java/cz/vut/fit/pisbackend/api/DrinksAPI.java
@@ -1,15 +1,14 @@
 package cz.vut.fit.pisbackend.api;
 
 import cz.vut.fit.pisbackend.api.dto.DrinkDTO;
+import cz.vut.fit.pisbackend.api.dto.ResponseMessageDTO;
+import cz.vut.fit.pisbackend.data.Drink;
 import cz.vut.fit.pisbackend.service.DrinkManager;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
 
+import java.net.URI;
 import java.util.List;
 
 @Path("drinks")
@@ -25,6 +24,27 @@ public class DrinksAPI {
     @Produces({ MediaType.APPLICATION_JSON })
     public List<DrinkDTO> getAll() {
         return drinkMgr.findAll().stream().map(x -> new DrinkDTO(x)).toList();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createDrink(DrinkDTO drink) {
+        Drink existing = drinkMgr.find(drink.getId());
+        if (existing == null){
+            Drink d = new Drink();
+            d.setName(drink.getName());
+            d.setDescription(drink.getDescription());
+            d.setPrice(drink.getPrice());
+            d.setType(drink.getType());
+            d.setVolume(drink.getVolume());
+            Drink savedDrink = drinkMgr.create(d);
+            final URI uri = UriBuilder.fromPath("/drinks/{resourceServerId}").build(savedDrink.getId());
+            return Response.created(uri).entity(savedDrink).build();
+        }
+        else{
+            return Response.status(Response.Status.CONFLICT).entity(new ResponseMessageDTO("duplicate id")).build();
+        }
     }
 
 }
